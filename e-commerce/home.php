7cb24f9067df8af0ac2bafc3d5d8741390cc6267
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-02-28 23:00:15 +0600

    e-commerce project ready

---
+++ e-commerce/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/responsive.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- Oswald font -->
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <title>Ecommerce Project</title>
+</head>
+<body>
+    <main class="main">
+    <?php include_once('./sections/header.php'); ?>
+    <?php include_once('./sections/banner.php'); ?>
+    </main>
+</body>
+</html>
---
+++ e-commerce/sections/header.php
@@ -0,0 +1,22 @@
+<header class="header-area">
+    <div class="container">
+        <div class="logo">
+            <img src="" alt="" class="logo">
+        </div>
+        <nav class="nav-area">
+            <!-- mobile menu -->
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Shop</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Pages</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div class="header-end">
+                <button></button>
+                <button></button>
+            </div>
+        </nav>
+    </div>
+</header>
