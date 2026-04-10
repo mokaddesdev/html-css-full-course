ef42392490363a0217d0d62f9b07df56a214b3bb
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-04-10 11:51:23 +0600

    flex box responsive image gallery

--- e-commerce/assets/js/main.js
+++ e-commerce/assets/js/main.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-   $(".banner-sliders").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-      infinite: true,
-      fade: true,
-      autoplay: true,
-      autoplaySpeed: 1500,
-   });
+// $(document).ready(function() {
+//    $(".banner-sliders").slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: true,
+//         arrows: false,
+//       infinite: true,
+//       fade: true,
+//       autoplay: true,
+//       autoplaySpeed: 1500,
+//    });
 
-   $(".banner-sliders").mouseover( function() {
-      $(this).slick('play')
-   });
-});
+//    $(".banner-sliders").mouseover( function() {
+//       $(this).slick('play')
+//    });
+// });
--- e-commerce/sections/banner.php
+++ e-commerce/sections/banner.php
@@ -10,23 +10,23 @@
             </div>
         </div>
 
-         <div class="single-banner">
+         <!-- <div class="single-banner">
             <div class="banner-description">
                 <h2>crispy, every bite taste</h2>
                 <h3>hot spicy chiken burger</h3>
                 <p>limited offer / <span>$5</span></p>
                 <button class="order-btn">order now</button>
             </div>
-        </div>
+        </div> -->
 
-        <div class="single-banner">
+        <!-- <div class="single-banner">
             <div class="banner-description">
                 <h2>crispy, every bite taste</h2>
                 <h3>chiken burger is now availabe</h3>
                 <p>50% discount / <span>$10</span></p>
                 <button class="order-btn">Buy Now</button>
             </div>
-        </div>
+        </div> -->
 
     </div>
 </section>
